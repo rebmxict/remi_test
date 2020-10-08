7a8ced44985d80ba87f4d8d9747f7c0ee2373f0b
fs45 <fs45.tech@gmail.com>
2020-10-08 16:08:54 +0800

    authentication implement

---
+++ account/api.py
@@ -0,0 +1,55 @@
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from knox.models import AuthToken
+from .serializers import UserSerializer, RegisterSerializer, LoginSerializer
+from .tokens import account_activation_token
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+import json
+
+# Register API
+class RegisterAPI(generics.GenericAPIView):
+
+	serializer_class = RegisterSerializer
+
+	def post(self, request, *args, **kwargs):
+		serializer = self.get_serializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		user = serializer.save()
+
+		token = AuthToken.objects.create(user)
+		if token.__class__.__name__ == 'tuple':
+			token = token[1]
+		return Response({
+			'user': UserSerializer(user, context=self.get_serializer_context()).data,
+			'token': token
+		})
+
+# Login API
+class LoginAPI(generics.GenericAPIView):
+
+	serializer_class = LoginSerializer
+
+	def post(self, request, *args, **kwargs):
+		serializer = self.get_serializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		user = serializer.validated_data
+
+		token = AuthToken.objects.create(user)
+		if token.__class__.__name__ == 'tuple':
+			token = token[1]
+		return Response({
+			'user': UserSerializer(user, context=self.get_serializer_context()).data,
+			'token': token
+		})
+
+
+# Get User API
+
+class UserAPI(generics.RetrieveAPIView):
+
+	permission_classes = [permissions.IsAuthenticated]
+	serializer_class = UserSerializer
+
+	def get_object(self):
+		userData = UserSerializer(self.request.user, context=self.get_serializer_context()).data
+		return userData
---
+++ account/serializers.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from rest_framework import serializers
+from django.core import serializers as core_serializers
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+import json
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, \
+    urlsafe_base64_decode
+from .tokens import account_activation_token
+
+
+# User Serializer
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = User
+        fields = ('id', 'username', 'email', 'is_active')
+
+
+# Register Serializer
+
+class RegisterSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = User
+        fields = '__all__'
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+
+    # Check email duplication
+
+        users = json.loads(core_serializers.serialize('json',
+                           User.objects.all()))
+        for user in users:
+            if validated_data['email'] in user['fields']['email']:
+                raise serializers.ValidationError('A EMAIL WITH THAT EMAIL ADDRESS ALREADY EXISTS.'
+                        )
+
+        user = User.objects.create_user(validated_data['username'],
+                validated_data['email'], validated_data['password'])
+        return user
+
+
+# Login Serializer
+
+class LoginSerializer(serializers.Serializer):
+
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError('Incorrect Credentials')
